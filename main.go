3d22930cef48f8f68e5c34d8d1fcf7bcd1e85a97
gitmobkab <richmondaurel77@gmail.com>
2026-01-02 21:58:45 +0000

    feat: implement random card category selection

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
+func pickRandomString(strs []string) string {
+	var random_index int = rand.IntN(len(strs))
+	return strs[random_index]
+}
+
+func pickRandomBalatroCardCategory() string{
+	cards_categories := []string{
+		"Jokers",
+		"Tarot Cards",
+		"Planet Cards",
+		"Spectral Cards",
+		"Vouchers",
+	}
+
+	return pickRandomString(cards_categories)
+}
+
 
 func main() {
-	fmt.Println("Hello from balafetch")
+	fmt.Println(pickRandomBalatroCardCategory())
 }
 
