edc2363306ecd86157b394ca2a72f35f917ce89d
gitmobkab <richmondaurel77@gmail.com>
2026-01-16 01:14:38 +0000

    feat: refactor main function to setup logging in a dedicated SetupLogFile function

--- cmd/balafetch/main.go
+++ cmd/balafetch/main.go
@@ -4,31 +4,63 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path"
 	"github.com/gitmobkab/balafetch/internal/run"
 )
 
-func main(){
-	logFile,fileErr := os.OpenFile("balafetch.log",os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+func SetupLogFile() (string, error){
 
-	if fileErr != nil {
-		log.Fatal(fileErr)
+	const BalafetchDirName string = "balafetch" 
+	const BalafetchLogFileName string = "balafetch.log"
+
+	UserHomeDir, UserHomeDirErr := os.UserHomeDir()
+	if UserHomeDirErr != nil {
+		fmt.Println(UserHomeDirErr)
+		return "", fmt.Errorf("Failed to get User Home Directory: %w", UserHomeDirErr)
 	}
 
+	BalafetchDirPath := path.Join(UserHomeDir, BalafetchDirName)
+	MkdirErr := os.MkdirAll(BalafetchDirPath, 0744) 
+	// ok, it's weird or maybe i don't get it
+	// but in order for us to open/create the log file,
+	// the user must explicitely possess the rwx rights
+	// so we don't make a dir with rw- permission
+	// but rather rwx
+	if MkdirErr != nil {
+		return "", fmt.Errorf("Failed to make Balafetch Dir: %w", MkdirErr)
+	}
+
+	logFilePath := path.Join(BalafetchDirPath, BalafetchLogFileName)
+	return logFilePath, nil
+}
+
+func main(){
+	const LogFileSetupFailureCode int = 50
+
+	logFilePath, logFileSetupErr := SetupLogFile()
+	if logFileSetupErr != nil{
+		fmt.Println("Balafetch setup failed\n",logFileSetupErr)
+		os.Exit(LogFileSetupFailureCode)
+	}
+	
+	logFile, OpenFileErr := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if OpenFileErr != nil{
+		fmt.Println("Unable to open log file:", OpenFileErr)
+		os.Exit(LogFileSetupFailureCode)
+	}
 
 	log.SetOutput(logFile)
-
-	exitCode, runErr := run.RunBalafetch()
-
-
+	
+	balafetchRunExitCode, balafetchRunErr := run.RunBalafetch()
 	switch {
-	case exitCode < run.CommandErrorCode:
+	case balafetchRunExitCode != run.SuccessCode && balafetchRunExitCode != run.CommandErrorCode:
 		run.RunFastfetchDefault()
-		log.Printf("[ Error Code: %d ]\n",exitCode)
-		log.Println(runErr)
-	case exitCode == run.CommandErrorCode:
+		log.Printf("[ Error Code: %d ]\n",balafetchRunExitCode)
+		log.Println(balafetchRunErr)
+	case balafetchRunExitCode == run.CommandErrorCode:
 		fmt.Println("Fastfetch not installed in your system")
 	}
 
 	logFile.Close()
-	os.Exit(exitCode)
+	os.Exit(balafetchRunExitCode)
 }
