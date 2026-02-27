80d13ffb9ccf3efdbdd2cab6e40e1204cfac2395
gitmobkab <richmondaurel77@gmail.com>
2026-02-27 02:06:20 +0000

    feat: refactor main function to utilize context management for API request handling

--- cmd/balafetch/main.go
+++ cmd/balafetch/main.go
@@ -4,25 +4,36 @@ import (
 	"os"
 	"github.com/gitmobkab/balafetch/internal/run"
 	"github.com/gitmobkab/balafetch/internal/exit_codes"
+	"github.com/gitmobkab/balafetch/internal/model"
 	pflag "github.com/spf13/pflag"
 )
 
 
 func main(){
+	var exitCode int = exitCodes.SuccessCode
+	
 	helpFlag := pflag.BoolP("help", "h", false, "Show help information")
 	versionFlag := pflag.BoolP("version", "v", false, "Show version information")
 	timeoutFlag := pflag.IntP("timeout", "t", 20, "Set the timeout for API requests in seconds")
-
 	pflag.Parse()
 
-	var exitCode int = exitCodes.SuccessCode
+	var app_ctx model.Ctx
+
+	card_category := pflag.Arg(0)
+	if card_category != "" {
+		app_ctx.CardCategory = card_category
+	} else {
+		app_ctx.CardCategory = ""
+	}
+
+	app_ctx.Timeout = *timeoutFlag
 
 	if *helpFlag {
 		Help()
 	} else if *versionFlag {
 		Version()
 	} else {
-		exitCode = run.FullBalafetchRun(*timeoutFlag)
+		exitCode = run.FullBalafetchRun(app_ctx)
 	}
 
 	os.Exit(exitCode)
