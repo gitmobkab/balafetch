54042267ba8b90aa5229ee6b3cf57b4a1c281a3e
gitmobkab <richmondaurel77@gmail.com>
2026-02-27 02:06:12 +0000

    feat: enhance help and version output for improved user guidance

--- cmd/balafetch/help.go
+++ cmd/balafetch/help.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	pflag "github.com/spf13/pflag"
+	"github.com/gitmobkab/balafetch/internal/data"
 )
 
 func Help() {
-	fmt.Println("Balafetch - The stupid balatro flavoured fastfetch wrapper")
-	fmt.Println("Usage: balafetch [options]")
+	fmt.Printf("%s - The stupid balatro flavoured fastfetch wrapper\n",data.AppName)
+	fmt.Printf("Usage: %s [options] [card_category]\n",data.AppName)
 	fmt.Println("Options:")
 	pflag.PrintDefaults()
+	fmt.Println("Card Categories:")
+	for _, category := range data.Cards_categories {
+		fmt.Printf(" - %s\n", category)
+	}
+	fmt.Println("\nNote: use double quotes for categories with spaces, e.g. \"tarot cards\"")
 }
