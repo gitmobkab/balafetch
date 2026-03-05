b850d05a7ae26446d1cfbc5fb4d127c58c39015c
gitmobkab <richmondaurel77@gmail.com>
2026-03-05 08:00:53 +0000

    refactor: update SetupLogFile to use a constant for log file permissions and remove unnecessary print statement

--- internal/log_file_helpers/log_setup.go
+++ internal/log_file_helpers/log_setup.go
@@ -8,23 +8,18 @@ import (
 )
 
 func SetupLogFile() (string, error){
+	LOG_FILE_PERM := os.FileMode.Perm(0744)
 
 	const BalafetchDirName string = "balafetch" 
 	const BalafetchLogFileName string = "balafetch.log"
 
 	UserHomeDir, UserHomeDirErr := os.UserHomeDir()
 	if UserHomeDirErr != nil {
-		fmt.Println(UserHomeDirErr)
 		return "", fmt.Errorf("Failed to get User Home Directory: %w", UserHomeDirErr)
 	}
 
 	BalafetchDirPath := path.Join(UserHomeDir, BalafetchDirName)
-	MkdirErr := os.MkdirAll(BalafetchDirPath, 0744) 
-	// ok, it's weird or maybe i don't get it
-	// but in order for us to open/create the log file,
-	// the user must explicitely possess the rwx rights
-	// so we don't make a dir with rw- permission
-	// but rather rwx
+	MkdirErr := os.MkdirAll(BalafetchDirPath, LOG_FILE_PERM) 
 	if MkdirErr != nil {
 		return "", fmt.Errorf("Failed to make Balafetch Dir: %w", MkdirErr)
 	}
