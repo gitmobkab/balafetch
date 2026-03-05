d054840e81d20440e0d4c2132d44fc40fe6c1688
gitmobkab <richmondaurel77@gmail.com>
2026-03-05 07:57:37 +0000

    refactor: improve documentation for Picker methods and clarify panic conditions

--- internal/random/pick.go
+++ internal/random/pick.go
@@ -17,16 +17,24 @@ func NewPicker(seed int64) *Picker {
 }
 
 
+/* 
+PickInt returns a random integer in the range [0, max). it panics if max <= 0.
+max is inclusive, so PickInt(1) will always return 0, PickInt(2) will return either 0 or 1, etc.
 
+Naively calls rng.Intn(max), expected to be called in a 'safe' environnement*/
 func (picker *Picker) PickInt(max int) int{
 	return picker.rng.Intn(max)
 }
 
+// PickRandomString returns a random string from the given slice. it panics if the slice is empty.
+// Naively calls PickInt(len(strs)) to get a random index, expected to be called in a 'safe' environnement
 func (picker *Picker) PickRandomString(strs []string) string {
 	var random_index int = picker.PickInt(len(strs))
 	return strs[random_index]
 }
 
+// PickRandomBalatroCardCategory returns a random category from the list of balatro card categories.
+// safest of the group, will only panic if the data.Cards_categories slice is empty.
 func (picker *Picker) PickRandomBalatroCardCategory() string{
 	return picker.PickRandomString(data.Cards_categories)
 }
