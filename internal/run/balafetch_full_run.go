7992b0eb1d555d197c09d6cafe92be607da23270
gitmobkab <richmondaurel77@gmail.com>
2026-03-05 08:02:49 +0000

    fix: improve error message formatting in FullBalafetchRun for better readability

--- internal/run/balafetch_full_run.go
+++ internal/run/balafetch_full_run.go
@@ -13,13 +13,13 @@ import (
 func FullBalafetchRun(ctx model.Ctx) (int){
 	logFilePath, logFileSetupErr := logfilehelpers.SetupLogFile()
 	if logFileSetupErr != nil{
-		fmt.Println("Balafetch setup failed\n",logFileSetupErr)
+		fmt.Printf("Balafetch setup failed:\n%s", logFileSetupErr)
 		os.Exit(exitCodes.LogFileSetupFailureCode)
 	}
 	
 	logFile, OpenFileErr := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if OpenFileErr != nil{
-		fmt.Println("Unable to open log file:", OpenFileErr)
+		fmt.Printf("Unable to open log file:\n%s", OpenFileErr)
 		os.Exit(exitCodes.LogFileSetupFailureCode)
 	}
 
