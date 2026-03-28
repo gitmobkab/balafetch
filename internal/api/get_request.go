7c778496aff0987ac872b5932999d7c10e794f19
gitmobkab <richmondaurel77@gmail.com>
2026-03-28 01:04:07 +0000

    refactor: Improved performance on GetRequest

--- internal/api/get_request.go
+++ internal/api/get_request.go
@@ -1,35 +1,42 @@
 package api
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"net/http"
 	"time"
 )
 
-func GetRequest(url string, request_timeout int) ([]byte, error) {
+var client = &http.Client{}
 
-	client := http.Client{
-		Timeout: time.Duration(request_timeout) * time.Second,
-	}
+// GetRequest is an utility function to execute a get request on the internet
+// a response not in the range 200-299 is flagged as bad and cause it to return an error
+func GetRequest(url string, requestTimeout int) ([]byte, error) {
 
-	resp, err := client.Get(url)
+	ctx := context.Background()
+	cancel := func() {}
+	if requestTimeout != 0 {
+		timeoutDuration := time.Duration(requestTimeout)*time.Second
+		ctx, cancel = context.WithTimeout(context.Background(), timeoutDuration.Abs())
+    }
+    defer cancel()
+
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+    if err != nil {
+        return nil, err
+    }
+
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
+	defer resp.Body.Close()
 	
 	if resp.StatusCode < 200 || resp.StatusCode > 299 {
 		return nil, fmt.Errorf("unexpected response status code: %d", resp.StatusCode)
 	}
 	
-	body, err := io.ReadAll(resp.Body) 
-	resp.Body.Close()
-	
-	if err != nil {
-		return nil, err
-
-	}
-
-	return body,nil
-
+	return io.ReadAll(resp.Body) 
 }
